f353553a78058aaa4b99ccadce41049930618e1b
FaisalAyed <fv99xz@hotmail.com>
2024-05-10 22:40:41 +0300

    update on sidebar

--- system/inc/sidebar.php
+++ system/inc/sidebar.php
@@ -159,34 +159,13 @@ $deb = [$db->DebShow(1), $db->DebShow(2), $db->DebShow(3), $db->DebShow(4), $db-
 <?php endif; ?>
 
 
-<?php
-// Define an associative array to map role_id to sidebar menu titles
-$sidebar_menus = array(
-    7 => $deb[1],   // قسم المشتريات
-    8 => $deb[2],   // ادارة الفعاليات
-    9 => $deb[3],   // إدارة المسؤولية الاجتماعية
-    10 => $deb[4],  // إدارة التسويق
-    11 => $deb[5],  // ادارة الاستثمار
-    12 => $deb[6],  // ادارة الالعاب المختلفة
-    13 => $deb[7],  // إدارة كرة القدم
-    14 => $deb[8],  // ادارة تقنية المعلومات
-    15 => $deb[9],  // إدارة العلاقات العامة
-    16 => $deb[10], // إدارة الإعلام والتواصل المجتمعي
-    17 => $deb[11], // الادارة المالية (المدير المالي)
-    21 => $deb[11], // الادارة المالية (المحاسب)
-    18 => $deb[12], // إدارة الموارد البشرية
-    19 => $deb[13], // إدارة الحوكمة والامتثال والمخاطر
-    20 => $deb[14]  // الادارة القانونية
-);
-
-// Check role_id and display corresponding sidebar menu
-if (isset($sidebar_menus[$role['role_id']])) {
-    ?>
+<!-- قسم المشتريات -->
+<?php if ($role['role_id'] == 7): ?>
     <ul id="sidebar_menu">
         <li>
             <a class="has-arrow" href="#" aria-expanded="false">
                 <i></i>
-                <span><?php echo $sidebar_menus[$role['role_id']]; ?></span>
+                <span><?php echo $deb[1] ?></span>
             </a>
             <ul>
                 <li><a class="active" href="#">test1</a></li>
@@ -194,6 +173,229 @@ if (isset($sidebar_menus[$role['role_id']])) {
             </ul>
         </li>
     </ul>
-    <?php
-}
-?>
+<?php endif; ?>
+
+<!-- ادارة الفعاليات -->
+<?php if ($role['role_id'] == 8): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[2] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- إدارة المسؤولية الاجتماعية -->
+<?php if ($role['role_id'] == 9): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[3] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- إدارة التسويق -->
+<?php if ($role['role_id'] == 10): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[4] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- ادارة الاستثمار -->
+<?php if ($role['role_id'] == 11): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[5] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- ادارة الالعاب المختلفة -->
+<?php if ($role['role_id'] == 12): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[6] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- إدارة كرة القدم -->
+<?php if ($role['role_id'] == 13): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[7] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- ادارة تقنية المعلومات -->
+<?php if ($role['role_id'] == 14): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[8] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- إدارة العلاقات العامة -->
+<?php if ($role['role_id'] == 15): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[9] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- إدارة الإعلام والتواصل المجتمعي -->
+
+<?php if ($role['role_id'] == 16): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[10] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- الادارة المالية -->
+<!-- المدير المالي -->
+<?php if ($role['role_id'] == 17): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[11] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+<!-- محاسب -->
+<?php if ($role['role_id'] == 21): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[11] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- إدارة الموارد البشرية -->
+<?php if ($role['role_id'] == 18): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[12] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- إدارة الحوكمة والامتثال والمخاطر -->
+<?php if ($role['role_id'] == 19): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[13] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
+
+<!-- الادارة القانونية -->
+<?php if ($role['role_id'] == 20): ?>
+    <ul id="sidebar_menu">
+        <li>
+            <a class="has-arrow" href="#" aria-expanded="false">
+                <i></i>
+                <span><?php echo $deb[14] ?></span>
+            </a>
+            <ul>
+                <li><a class="active" href="#">test1</a></li>
+                <li><a href="#">test2</a></li>
+            </ul>
+        </li>
+    </ul>
+<?php endif; ?>
